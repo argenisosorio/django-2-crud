f762941adcc008282c28e10c85e4755541523eb4
Argenis Osorio <argenisosorio580@hotmail.com>
2018-02-18 12:46:13 -0400

    The first app was added

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for mysite project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
